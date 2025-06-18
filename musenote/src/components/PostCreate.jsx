ab63cc8baeccb6ae2b4a91b609c6a9bc82bf0c58
Nainika-V <nainikavempalli@gmail.com>
2025-06-18 15:06:37 +0530

    Added tags, genre in addPost

--- musenote/src/components/PostCreate.jsx
+++ musenote/src/components/PostCreate.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { generatePath, Link } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 import logo from '../assets/logo.png';
 import './PostCreate.css';
@@ -7,6 +7,9 @@ import './PostCreate.css';
 const PostCreate = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [tag1, setTag1] = useState('');
+  const [tag2, setTag2] = useState('');
+  const [genre, setGenre] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -53,6 +56,33 @@ const PostCreate = () => {
             onChange={(e) => setContent(e.target.value)}
             required
           ></textarea>
+
+          <label className="tag1">Tag-1</label>
+          <input
+            type="text"
+            className="input-field"
+            value={tag1}
+            onChange={(e) => setTag1(e.target.value)}
+            required
+          />
+
+          <label className="tag2">Tag-2</label>
+          <input
+            type="text"
+            className="input-field"
+            value={tag2}
+            onChange={(e) => setTag2(e.target.value)}
+            required
+          />
+
+          <label className="genre">Genre</label>
+          <input
+            type="text"
+            className="input-field"
+            value={genre}
+            onChange={(e) => setGenre(e.target.value)}
+            required
+          />
 
           <button type="submit" className="submit-button">Post</button>
         </form>
